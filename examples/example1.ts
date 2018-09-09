34317c0185b036477c8945e7540969973eb79cf7
Anders Westberg <anders.westberg@diginet.se>
2018-09-09 16:28:15 +0200

    Minor changes to example

--- examples/example1.ts
+++ examples/example1.ts
@@ -2,9 +2,9 @@ import { CorsProxy } from "../CorsProxy"
 import * as parseArgs from "minimist"
 
 const argv = parseArgs(process.argv)
-let host: string = argv.h ? argv.h.trim() : "127.0.0.1"
+let ip: string = argv.ip ? argv.ip.trim() : "127.0.0.1"
 let port: number = argv.p ? parseInt((argv.p).trim()) : 8787
-const corsProxy = new CorsProxy(host, port, console.log)
+const corsProxy = new CorsProxy(ip, port, console.log)
 setTimeout(() => {
 
 }, 100000)
